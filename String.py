ab67da79c8c8e62ee12b4bb2a96880c88f3d5d10
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-11-15 22:38:13 +0530

    learned how to use strings

---
+++ String.py
@@ -0,0 +1,16 @@
+print("Today is a good day to learn Python")
+print("Python is fun")
+print("Python's string are easy to use")
+print("We can even conclude quotes in strings")
+print("hello" + " "+ "world")
+greeting = "hello"
+name = input("Please enter your name:")
+print(greeting + " " + name)
+age = 24
+print(age)
+print(type(greeting))
+print(type(age))
+age_in_words = "2 years"
+print(type(age))
+print(name + "is" + age + "years old")
+print(type(age))
