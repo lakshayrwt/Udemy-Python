45614e423e9f58b32e18b5dba7baf29cf4232449
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-18 22:44:31 +0530

    did some questions on if,elif and else

--- Program-Flow.py/guessing_game.py
+++ Program-Flow.py/guessing_game.py
@@ -18,3 +18,11 @@ elif x < y:
     print("x is smaller than y")
 elif x == y:
     print("x equals y")
+
+tree1 = "Adam"
+tree2 = "Eve"
+
+if tree1 == tree2 :
+    print("The trees are the same")
+else:
+    print("The trees are different")
