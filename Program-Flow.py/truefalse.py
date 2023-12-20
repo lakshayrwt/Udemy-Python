447ac6419bea1b90d6ddac4db87ee3bcf18d5e3c
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-20 20:14:56 +0530

    used AND and OR in same code

--- Program-Flow.py/truefalse.py
+++ Program-Flow.py/truefalse.py
@@ -2,7 +2,7 @@ day = "Saturday"
 temperature = 30
 raining = False
 
-if day == "Saturday" and temperature > 30 or not raining:
+if (day == "Saturday" and temperature > 30) or not raining:
     print("go play basketball")
 else:
     print("learn Python")
