67256e1078baf0239c246d931a31a9d231df72b5
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-20 19:27:20 +0530

    learned how to use AND operator

---
+++ Program-Flow.py/conditions.py
@@ -0,0 +1,8 @@
+age = int(input("how old are sir? "))
+if age >= 16 and age <=60:
+    print("go work till death")
+else:
+    if age < 16:
+        print("too young brat")
+    elif age > 60:
+        print("now embrace death")
