de812703f14ceca964613b7fb9d6b44be4617e91
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-20 20:27:58 +0530

    tried truthh and false values

--- Program-Flow.py/truefalse.py
+++ Program-Flow.py/truefalse.py
@@ -6,3 +6,14 @@ if (day == "Saturday" and temperature > 30) or not raining:
     print("go play basketball")
 else:
     print("learn Python")
+
+if 0:
+    print("true")
+else:
+    print("false")
+
+name = input("please enter your name? ")
+if name:
+    print("hello, {0}".format(name))
+else:
+    print("are you the man with no name")
