af92d5816d6b5e895924196c324ca6d295c3caba
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-29 19:48:01 +0530

    did a challenge on if

---
+++ Program-Flow.py/ifchallenge.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+if age >= 18 and age < 31:
+    print("Enjoy your Holiday")
+else:
+    print("Sorry,but you have to be over 18")
