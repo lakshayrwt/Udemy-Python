880f9c76aa3e6fbeebfc0fafe11835ed7bed1e7a
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-29 20:41:32 +0530

    used some more for loops

--- Program-Flow.py/forloops.py
+++ Program-Flow.py/forloops.py
@@ -1,3 +1,10 @@
 parrot = "Norwegian Blue"
 for character in parrot:
     print(character)
+
+number = input("Please enter a value of your choise:")
+seperators = ""
+for char in number:
+    if not char.isnumeric():
+        seperators = seperators + char
+print(seperators)
