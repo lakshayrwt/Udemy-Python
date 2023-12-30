b81355e3b667d858a6f50bbc3efee123e86b8414
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-31 00:08:19 +0530

    used break in while loop

--- Program-Flow.py/adventure.py
+++ Program-Flow.py/adventure.py
@@ -2,5 +2,8 @@ available_exits = ["north","south","ease","west"]
 chosen_exits = ""
 while chosen_exits not in available_exits:
     chosen_exits = input("So,which way do you want to go,Pal:")
+    if chosen_exits.casefold() == "quit":
+        print("Game Over")
+        break
 
 print("aren't you glad we didn't chose the other ways")
