5fdaefa54c297f7e509d7ac97219a2cb81d4c65b
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-30 22:22:19 +0530

    modified the code a bit

--- Program-Flow.py/searching.py
+++ Program-Flow.py/searching.py
@@ -1,9 +1,16 @@
 shopping_list = ["milk","pasta","eggs","spam","bread","rice"]
 
-item_to_find = "spam"
+item_to_find = "albatross"
 found_at = None
-for index in range(len(shopping_list)):
-    if shopping_list[index] == item_to_find:
-        found_at = index
-        break
-print("Item found at {}".format(found_at))
+# for index in range(len(shopping_list)):
+#     if shopping_list[index] == item_to_find:
+#         found_at = index
+#         break
+
+if item_to_find in shopping_list:
+    found_at = shopping_list.index(item_to_find)
+    
+if found_at is not None:
+    print("item found at {}".format(found_at))
+else:
+    print("{} not found".format(item_to_find))
