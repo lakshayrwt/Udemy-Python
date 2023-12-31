763026f011fa1a1dbc5f7769e80e4d72addb999c
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-31 19:16:23 +0530

    modified the challenge with while loop

--- Program-Flow.py/section_challenge.py
+++ Program-Flow.py/section_challenge.py
@@ -1,9 +1,6 @@
 choise = "-"
-while True:
-    if choise == "6":
-        print("Please find a life goal")
-        break
-    elif choise in "12345":
+while choise != "6":
+    if choise in "12345":
         print("You chose {}".format(choise))
     else:
         print("Please chose from the options below")
