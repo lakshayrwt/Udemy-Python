bb3fa16988969078eebac387859c1be086a6db3e
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-31 19:13:25 +0530

    modified the challenge with while loop

--- Program-Flow.py/section_challenge.py
+++ Program-Flow.py/section_challenge.py
@@ -1,13 +1,5 @@
-print("Please chose from the options below")
-print("1:\t Doctor")
-print("2:\t Engineer")
-print("3:\t Pilot")
-print("4:\t Marine")
-print("5:\t Influencer")
-print("6:\tQuit")
-
+choise = "-"
 while True:
-    choise = input()
     if choise == "6":
         print("Please find a life goal")
         break
@@ -21,3 +13,5 @@ while True:
         print("4:\t Marine")
         print("5:\t Influencer")
         print("6:\tQuit")
+
+    choise = input()
