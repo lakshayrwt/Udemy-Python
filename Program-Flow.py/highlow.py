8180162d808e074872ea54d5605519bbc3fbf45b
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-31 14:38:45 +0530

    made the guessing game more interesting

---
+++ Program-Flow.py/highlow.py
@@ -0,0 +1,23 @@
+low = 1
+high = 1000
+
+print("Please think of a number between {} and {}".format(low , high))
+input("Please ENTER to start")
+
+guesses = 1
+while True:
+    print("\t guess in the range of {} and {}".format(low, high))
+    guess = low + (high - low) // 2
+    high_low = input("My guess is {}. should i guess higher or lower? Enter h or l, or c if my guess is correct ".format(guess)).casefold()
+
+    if high_low == "h":
+        low = guess + 1
+    elif high_low == "l":
+        high = guess - 1
+    elif high_low == "c":
+        print("i got it in {} guesses".format(guesses))
+        break
+    else:
+        print("Please enter h , l or c")
+
+    guesses = guesses + 1  
