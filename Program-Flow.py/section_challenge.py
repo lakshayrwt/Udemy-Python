b575b9495752a69e215474264fb2079e151304ae
Lakshay Rawat <rawatlakshay38@gmail.com>
2023-12-31 18:49:31 +0530

    did an section end challenge in my own way

---
+++ Program-Flow.py/section_challenge.py
@@ -0,0 +1,19 @@
+career_options ="""
+1. Doctor
+2. Engineer
+3. Pilot
+4. Marine
+5. Influencer
+"""
+print(career_options)
+for future_option in career_options:
+    future_option = input("Please select any option from the list:")
+    print(future_option)
+    if future_option in career_options:
+        print("May you succeed in life")
+        break
+    elif future_option == "quit":
+        print("Please find a life goal")
+        break
+    else:
+        print("Please select from the list given")
