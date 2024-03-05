ce45b7299993f475c180fb6c360f790b262c6c33
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-05 23:14:49 +0530

    checked that str and bool are immutable in nature

---
+++ Sequences.py/immutable.py
@@ -0,0 +1,21 @@
+result = True
+another_result = result
+print(id(result))
+print(id(another_result))
+
+print()
+
+result = False
+print(id(result))
+
+print()
+
+result = "correct"
+another_result = result
+print(id(result))
+print(id(another_result))
+
+print()
+
+result += "ish"
+print(id(result))
