9fd148a1c45e98db78f04a5b8f6f49b6ce298200
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-05 22:55:28 +0530

    performed slicing and indexing on lists

---
+++ Sequences.py/list_intro.py
@@ -0,0 +1,10 @@
+computer_parts = ["computer" , "monitor" , "keyboard" , "mouse" , "mouse mat"]
+
+for parts in computer_parts:
+    print(parts)
+
+print()
+
+print(computer_parts[2])
+print(computer_parts[0:3])
+print(computer_parts[-1])
