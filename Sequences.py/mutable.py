e771034b3140b7e0956793be1aa78037182d04da
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-05 23:28:26 +0530

    checked that lists are mutable in nature

---
+++ Sequences.py/mutable.py
@@ -0,0 +1,10 @@
+shopping_list = ["milk","pasta","eggs","spam","bread","rice"]
+
+another_list = shopping_list
+
+print(id(shopping_list))
+print(id(another_list))
+
+shopping_list += ["cookies"]
+
+print(id(shopping_list))
