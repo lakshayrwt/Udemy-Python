cbe47a3420623ce6097876749220c423de8911f5
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-07 00:26:14 +0530

    made the buying menu more efficient

--- Sequences.py/buy_computer.py
+++ Sequences.py/buy_computer.py
@@ -8,22 +8,17 @@ available_parts = ["monitor",
 current_choice = "-"
 computer_parts = []
 
-while current_choice != "0":
-    if current_choice in "1,2,3,4,5,6":
-        print("adding {}".format(current_choice))
-        if current_choice == '1':
-            computer_parts.append("monitor")
-        elif current_choice == '2':
-            computer_parts.append("CPU")
-        elif current_choice == '3':
-            computer_parts.append("keyboard")
-        elif current_choice == '4':
-            computer_parts.append("mouse")
-        elif current_choice == '5':
-            computer_parts.append("mouse pad")
-        elif current_choice == '6':
-            computer_parts.append("HDMI cable")
+valid_choices = []
+for i in range(1,len(available_parts) + 1):
+    valid_choices.append(str(i))
+print(valid_choices)
 
+while current_choice != "0":
+    if current_choice in valid_choices:
+        print("adding {}".format(current_choice))
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
     else:
         print("select from the following options")
         for number, part in enumerate(available_parts):
