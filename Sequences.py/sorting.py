5732be77f02d819e366132dfb48e044905665524
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-07 23:37:27 +0530

    difference btw sort and sorted

---
+++ Sequences.py/sorting.py
@@ -0,0 +1,12 @@
+pangram = "The quick brown fox jumps over the lazy dog"
+letters = sorted(pangram)
+print(letters)
+
+numbers = [2.5 , 8.1 , 3.9 , 4.8 , 5.5 , 6.2 , 7.3]
+another_numbers = sorted(numbers)
+print(numbers)
+print(another_numbers)
+
+numbers.sort()
+print(numbers)
+
