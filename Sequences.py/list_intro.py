6a4a20aff29d66dc9b792fa6bb5b5b3e681774af
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-08 23:09:41 +0530

    added and removed items from the menu

--- Sequences.py/list_intro.py
+++ Sequences.py/list_intro.py
@@ -1,10 +1,16 @@
 computer_parts = ["computer" , "monitor" , "keyboard" , "mouse" , "mouse mat"]
 
-for parts in computer_parts:
-    print(parts)
+# for parts in computer_parts:
+#     print(parts)
 
-print()
+# print()
 
-print(computer_parts[2])
-print(computer_parts[0:3])
-print(computer_parts[-1])
+# print(computer_parts[2])
+# print(computer_parts[0:3])
+# print(computer_parts[-1])
+computer_parts[4] = "speakers"
+print(computer_parts)
+computer_parts[0:2] = "cpu","T.V"
+print(computer_parts)
+del computer_parts[3]
+print(computer_parts)
