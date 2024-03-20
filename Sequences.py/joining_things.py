8e80d9e3be2ddf4a3a9cf5d831c7534b8e452b24
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-20 21:57:02 +0530

    learned how to join lists

---
+++ Sequences.py/joining_things.py
@@ -0,0 +1,14 @@
+Anime = [
+    "Attack on titan",
+    "One Piece",
+    "Naruto",
+    "Bleach",
+    "Dragon Ball Z",
+    "My Hero Academia",
+    "Hunter X Hunter"
+]
+separators = " | "
+output = separators.join(Anime)
+print(output)
+
+print(" , ".join(Anime))
