85019fa5272c3e087bacbddb5ffc7c81b9666343
Lakshay Rawat <rawatlakshay38@gmail.com>
2024-03-21 15:17:43 +0530

    unpacking tuples and lists

---
+++ Sequences.py/unpacking.py
@@ -0,0 +1,13 @@
+a = b = c = d = e = f = 42
+print(a,b,c,sep=",")
+
+x,y = 1,2
+print(x,y,sep=",")
+
+print("unpacking a list")
+
+data_list = [29,9,7]
+D,M,Y = data_list
+print(D)
+print(M)
+print(Y)
